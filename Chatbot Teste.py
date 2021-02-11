73e7df899b6ea3ad1d5bcbc126b771ff55c1d147
Roh_Sousa <44953964+Rodrigo1809@users.noreply.github.com>
2021-02-11 18:44:35 -0300

    Add files via upload

---
+++ Chatbot Teste.py
@@ -0,0 +1,12 @@
+#app = input
+print('Chatbot Teste')
+name = input('What is your name?\n') 
+print('Hello, %s!' % name)
+print('I m Misty')
+hau = input ('How are you?\n ')
+print('I m fine') 
+age = input("What is your age?\n")
+print('Me Too') 
+perg1 = input("What are you doing?\n")
+
+Author = input("Author: Roh Sousa\n")
